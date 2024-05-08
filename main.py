2cbcb5d400a3951a818aec9a9e3d73b3545ed092
cleverpanther <cleverpanther0819@gmail.com>
2024-05-08 12:03:09 -0700

    env update

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ import os
 
 load_dotenv()
 
-api_key = os.getenv('SECRET_KEY')
-otis_pwd = os.getenv('OTIS_PASSWORD')
-bing_pwd = os.getenv('BING_PASSWORD')
+SECRET_KEY = os.getenv('SECRET_KEY')
+OTIS_PASSWORD = os.getenv('OTIS_PASSWORD')
+BING_PASSWORD = os.getenv('BING_PASSWORD')
+
+print(BING_PASSWORD)
+print(OTIS_PASSWORD)
+
 db_config = {'user': 'root', 'password': '', 'host': 'localhost', 'database': 'chatbot', 'raise_on_warnings': True}
 
-# bing_email_handler = EmailHandler('emmyandbing@gmail.com', 'Korgi123', db_config, api_key, 'bing')
-otis_email_handler = EmailHandler('otis1880town@gmail.com', 'ifck bkvk pjjp sgdm', db_config, api_key, 'otis')
+bing_email_handler = EmailHandler('emmyandbing@gmail.com', BING_PASSWORD, db_config, SECRET_KEY, 'bing')
+otis_email_handler = EmailHandler('otis1880town@gmail.com', OTIS_PASSWORD, db_config, SECRET_KEY, 'otis')
 
 def job():
-    # print("Checking emails for Bing...")
-    # bing_email_handler.read_emails_bing()
+    print("Checking emails for Bing...")
+    otis_email_handler.reconnect()
+    bing_email_handler.read_emails_bing()
     print("Checking emails for Otis...")
     otis_email_handler.reconnect()
     otis_email_handler.read_emails_otis()
